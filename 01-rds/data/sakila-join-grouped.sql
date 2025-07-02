81cd6b88e9bfc489dc88e7ef096d84d00de5cd45
Mike Monaco <mamonaco1973@gmail.com>
2025-07-02 09:35:58 -0400

    convert postgre queries to mysql

--- 01-rds/data/sakila-join-grouped.sql
+++ 01-rds/data/sakila-join-grouped.sql
@@ -1,11 +1,10 @@
 SELECT
     f.title,
-    STRING_AGG(a.first_name || ' ' || a.last_name, ', ') AS actor_names
+    GROUP_CONCAT(CONCAT(a.first_name, ' ', a.last_name) ORDER BY a.last_name SEPARATOR ', ') AS actor_names
 FROM
-    film f
-JOIN film_actor fa ON f.film_id = fa.film_id
-JOIN actor a ON fa.actor_id = a.actor_id
+    sakila.film f
+JOIN sakila.film_actor fa ON f.film_id = fa.film_id
+JOIN sakila.actor a ON fa.actor_id = a.actor_id
 GROUP BY f.title
 ORDER BY f.title
 LIMIT 10;
-
--- 01-rds/data/sakila-join.sql
+++ 01-rds/data/sakila-join.sql
@@ -1,9 +1,9 @@
 SELECT
     f.title AS film_title,
-    a.first_name || ' ' || a.last_name AS actor_name
+    CONCAT(a.first_name, ' ', a.last_name) AS actor_name
 FROM
-    film f
-JOIN film_actor fa ON f.film_id = fa.film_id
-JOIN actor a ON fa.actor_id = a.actor_id
+    sakila.film f
+JOIN sakila.film_actor fa ON f.film_id = fa.film_id
+JOIN sakila.actor a ON fa.actor_id = a.actor_id
 ORDER BY f.title, actor_name
 LIMIT 20;
