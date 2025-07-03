14da42cd4d8cee9d55d9906235db1cce4c3469a2
Mike Monaco <mamonaco1973@gmail.com>
2025-07-03 11:26:02 -0400

    add aurora instance

--- 01-rds/phpmyadmin.tf
+++ 01-rds/phpmyadmin.tf
@@ -1,6 +1,6 @@
 # App Runner service
-resource "aws_apprunner_service" "phpmyadmin" {
-  service_name = "phpmyadmin"
+resource "aws_apprunner_service" "phpmyadmin_rds" {
+  service_name = "phpmyadmin-rds"
 
   source_configuration {
 
@@ -8,7 +8,7 @@ resource "aws_apprunner_service" "phpmyadmin" {
 
     image_repository {
       image_identifier      = "public.ecr.aws/docker/library/phpmyadmin:latest"
-      image_repository_type = "ECR_PUBLIC" # Works for Docker Hub
+      image_repository_type = "ECR_PUBLIC" 
 
       image_configuration {
         port = "80"
@@ -34,7 +34,44 @@ resource "aws_apprunner_service" "phpmyadmin" {
   }
 }
 
-# Outputs
-output "apprunner_service_url" {
-  value = aws_apprunner_service.phpmyadmin.service_url
+
+# App Runner service
+resource "aws_apprunner_service" "phpmyadmin_aurora" {
+  service_name = "phpmyadmin-aurora"
+
+  source_configuration {
+
+    auto_deployments_enabled = false
+
+    image_repository {
+      image_identifier      = "public.ecr.aws/docker/library/phpmyadmin:latest"
+      image_repository_type = "ECR_PUBLIC"
+
+      image_configuration {
+        port = "80"
+        runtime_environment_variables = {
+          PMA_HOST     = split(":", aws_rds_cluster.aurora_cluster.endpoint)[0]
+        }
+      }
+    }
+  }
+
+  instance_configuration {
+    cpu    = "1024" # 1 vCPU
+    memory = "2048" # 2GB RAM
+  }
+
+  health_check_configuration {
+    protocol            = "TCP"
+    path                = "/"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 1
+    unhealthy_threshold = 5
+  }
 }
+
+# # Outputs
+# output "apprunner_service_url" {
+#   value = aws_apprunner_service.phpmyadmin.service_url
+# }
